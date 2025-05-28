f9c6bc002f05d3952049c061ce332b4bd7e5b7e6
Sabit Murzaliev <sabit.murzaliev@gmail.com>
2025-05-28 14:29:27 +0500

    update

--- src/main/java/kz/sab1tm/hello/HelloApplication.java
+++ src/main/java/kz/sab1tm/hello/HelloApplication.java
@@ -2,22 +2,12 @@ package kz.sab1tm.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class HelloApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
     }
 
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 }
